36755c4d0aace0683a72a9f33ecc8f8849e065fd
@Coder2100 <mtotsoz@gmail.com>
2019-06-09 04:38:25 +0200

    bug fixing in user plan type

--- iStream/urls.py
+++ iStream/urls.py
@@ -30,5 +30,10 @@ urlpatterns = [
     path("", include("accounts.urls")),
     path("", include("entertainments.urls")),
    # path('profile/', profile_view, name='profile'),
-]+static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
 
--- plans/models.py
+++ plans/models.py
@@ -1,21 +1,22 @@
 from django.conf import settings
 from django.db import models
-
 from django.db.models.signals import post_save
+
 from datetime import datetime
 import stripe
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
-# Create your models here.
+
 PLAN_CHOICES = (
-    ('Enterprise', 'Enterpise'),
-    ('Professional', 'Professional'),
-    ('Free', 'Free')
+    ('Enterprise', 'ent'),
+    ('Professional', 'pro'),
+    ('Free', 'free')
 )
+
 
 class Plan(models.Model):
     slug = models.SlugField()
-    plan_type = models.CharField(
+    plan_type=models.CharField(
         choices=PLAN_CHOICES,
         default='Free',
         max_length=30)
@@ -27,44 +28,52 @@ class Plan(models.Model):
 
 
 class UserPlan(models.Model):
-    user = user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     stripe_customer_id = models.CharField(max_length=40)
     plan = models.ForeignKey(
         Plan, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
-        return f"{self.user.username}"
+        return self.user.username
+
 
 def post_save_userplan_create(sender, instance, created, *args, **kwargs):
-    user_plan, creted = UserPlan.objects.get_or_create(user=instance)
-#logic to default Plan option to free if the user has not subscribed to any plan
+    user_plan, created = UserPlan.objects.get_or_create(
+        user=instance)
+
     if user_plan.stripe_customer_id is None or user_plan.stripe_customer_id == '':
         new_customer_id = stripe.Customer.create(email=instance.email)
-        free_plan = Membership.objects.get(membership_type='Free')
+        try:
+            free_plan = Plan.objects.get(plan_type='Free')
+        except Plan.DoesNotExist:
+            free_plan = None
         user_plan.stripe_customer_id = new_customer_id['id']
-        user_plan.membership = free_plan
+        user_plan.plan = free_plan
         user_plan.save()
 
-post_save.connect(post_save_userplan_create, sender=settings.AUTH_USER_MODEL)
 
+post_save.connect(post_save_userplan_create,
+                  sender=settings.AUTH_USER_MODEL)
 
 
 class Subscription(models.Model):
-    user_plan = models.ForeignKey(UserPlan, on_delete=models.CASCADE)
+    user_plan = models.ForeignKey(
+        UserPlan, on_delete=models.CASCADE)
     stripe_subscription_id = models.CharField(max_length=40)
-    activate = models.BooleanField(default=True)
+    active = models.BooleanField(default=True)
 
     def __str__(self):
-        return f"{self.user_plan.user.username}"
-@property
-def get_created_date(self):
-    subscription = stripe.Subscription.retrieve(self.stripe_subscription_id)
-    return datetime.fromtimestamp(subscription.created)
+        return self.user_plan.user.username
 
-@property
-def get_next_billing_date(self):
-    subscription = stripe.Subscription.retrieve(self.stripe_subscription_id)
-    return datetime.fromtimestamp(subscription.current_period_end)
+    @property
+    def get_created_date(self):
+        subscription = stripe.Subscription.retrieve(
+            self.stripe_subscription_id)
+        return datetime.fromtimestamp(subscription.created)
 
-
-    
+    @property
+    def get_next_billing_date(self):
+        subscription = stripe.Subscription.retrieve(
+            self.stripe_subscription_id)
+        return datetime.fromtimestamp(subscription.current_period_end)
